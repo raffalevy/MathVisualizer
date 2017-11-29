ea3a8b1de239df7f488b0ac07c381d97d76ab683
Raphael Levy <student@Raphael-Levy.local>
2017-11-28 19:27:35 -0500

    Added field line plotting

--- client/ts/Plotter.tsx
+++ client/ts/Plotter.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
-import {ReactElement, ReactHTMLElement} from "react";
+import { ReactElement, ReactHTMLElement } from "react";
 
 import * as math from 'mathjs';
 
+/**
+ * Parameters representing how a parametric function is to be drawn
+ */
 export interface ParametricFunctionParams {
     p: ParametricFunction,
     pStart: number,
@@ -10,10 +13,24 @@ export interface ParametricFunctionParams {
     pStep: number
 }
 
+/**
+ * Used to transform between plot coordinates and screen coordinates
+ */
 export class CoordinateSystem {
 
+    /**
+     * The X coordinate of the origin
+     */
     readonly originX: number;
+
+    /**
+     * The Y coordinate of the origin
+     */
     readonly originY: number;
+
+    /**
+     * The number of pixels which cooresponds to one unit on the plot
+     */
     readonly unit: number;
 
     constructor(originX: number, originY: number, unit: number) {
@@ -22,28 +39,51 @@ export class CoordinateSystem {
         this.unit = unit;
     }
 
+    /**
+     * Transform plot X to screen X
+     * @param inX 
+     */
     x(inX: number): number {
         return this.unit * inX + this.originX;
     }
 
+    /**
+     * Transform plot Y to screen Y
+     * @param inY 
+     */
     y(inY: number): number {
         return Plotter.HEIGHT - (this.unit * inY + this.originY);
     }
 
+    /**
+     * Transform screen X to plot X
+     * @param inX 
+     */
     invX(inX: number): number {
         return (inX - this.originX) / this.unit;
     }
 
+    /**
+     * Transform screen Y to plot Y
+     * @param inY 
+     */
     invY(inY: number) {
         return (Plotter.HEIGHT - inY - this.originY) / this.unit;
     }
 
-    withUnit(newUnit) : CoordinateSystem {
+    /**
+     * Return an identical coordinate system to this one using a different unit
+     * @param newUnit
+     */
+    withUnit(newUnit): CoordinateSystem {
         return new CoordinateSystem(this.originX, this.originY, newUnit);
     }
 
 }
 
+/**
+ * Represents a 2d parametric function
+ */
 export class ParametricFunction {
     x: (number) => number;
     y: (number) => number;
@@ -66,11 +106,19 @@ export class Vector2D {
         this.y = y;
     }
 
-    magnitude() : number {
-        return math.sqrt(this.x**2 + this.y**2);
+    /**
+     * Get this vector's magnitude on the Euclidean plane
+     */
+    magnitude(): number {
+        return math.sqrt(this.x ** 2 + this.y ** 2);
     }
 
-    transform(f: (x: number) => number, g? : (y: number) => number) : Vector2D {
+    /**
+     * Apply a function to the components of this vector
+     * @param f The transformation to apply to the X component. This will also be applied to the Y component if no second function is given.
+     * @param g The transformation to apply to the Y component.
+     */
+    transform(f: (x: number) => number, g?: (y: number) => number): Vector2D {
         if (!g) {
             g = f;
         }
@@ -78,28 +126,36 @@ export class Vector2D {
         return new Vector2D(f(this.x), g(this.y));
     }
 
-    unitVector() : Vector2D {
+    /**
+     * Returns a vector pointing in the same direction as this one with a magnitude of 1.
+     */
+    unitVector(): Vector2D {
         return this.transform((x) => x / this.magnitude());
     }
 
     /**
-     * Scalar multiplication
-     * @param {number} a
+     * Multiply this vector by a scalar
+     * @param {number} a The scalar to multiply by
      */
-    times(a : number) : Vector2D{
+    times(a: number): Vector2D {
         return this.transform(x => x * a);
     }
 
     /**
-     * Vector addition
-     * @param {Vector2D} v
-     * @returns {Vector2D}
+     * Add another vector to this one
+     * @param {Vector2D} v The vector to add
+     * @returns {Vector2D} The sum of the two vectors
      */
-    plus(v : Vector2D) : Vector2D {
+    plus(v: Vector2D): Vector2D {
         return this.transform(x => x + v.x, y => y + v.y);
     }
 
-    minus(v: Vector2D) : Vector2D {
+    /**
+     * Subtract a vector from this one
+     * @param v The vector to subtrace
+     * @returns {Vector2D} The difference of the vectors
+     */
+    minus(v: Vector2D): Vector2D {
         return this.plus(v.times(-1));
     }
 }
@@ -113,7 +169,7 @@ export interface VectorField {
  */
 export namespace VectorField {
     export function add(f: VectorField, g: VectorField) {
-        return (x,y) => f(x,y).plus(g(x,y));
+        return (x, y) => f(x, y).plus(g(x, y));
     }
 
     export function subtract(f: VectorField, g: VectorField) {
@@ -121,23 +177,54 @@ export namespace VectorField {
     }
 
     export function multiply(f: VectorField, a: number) {
-        return (x,y) => f(x,y).times(a);
+        return (x, y) => f(x, y).times(a);
     }
 }
 
+export interface VectorFieldParams {
+    field: VectorField
+    fieldLines?: [number, number][]
+    fieldLineStepSize?: number
+    fieldLineSteps?: number
+}
+
 export interface PlotterProps {
+    /**
+     * The parametric functions to plot
+     */
     parametricFunctions?: ParametricFunctionParams[]
-    vectorFields?: VectorField[]
+
+    /**
+     * The vector fields to plot
+     */
+    vectorFields?: VectorFieldParams[]
+
+    /**
+     * The number of pixels which corresponds to one unit in plot coordinates
+     */
     unit?: number
+
+    /**
+     * An array of points to plot
+     */
+    plotPoints?: [number, number][]
+
+    /**
+     * Called when the mouse is moved over the canvas
+     */
+    onMouseMove?: (x: number, y: number) => void
 }
 
 interface PlotterState {
     ctx: CanvasRenderingContext2D,
     cs: CoordinateSystem,
     parametricFunctions?: ParametricFunctionParams[]
-    vectorFields?: VectorField[]
+    vectorFields?: VectorFieldParams[]
 }
 
+/**
+ * A component which uses a canvas to plot functions and vectors
+ */
 export class Plotter extends React.Component<PlotterProps, PlotterState> {
 
     static readonly WIDTH = 600;
@@ -149,6 +236,9 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
 
     static readonly VECTOR_LENGTH = 10;
 
+    static readonly FIELD_LINE_STEP_FACTOR = .1;
+    static readonly FIELD_LINE_STEPS = 100;
+
     static readonly CANVAS_STYLE = {
         border: "solid 1px gray",
         width: Plotter.WIDTH.toString() + "px",
@@ -156,7 +246,7 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
     };
 
     canvas = undefined;
-    canvasRef = undefined;
+    canvasRef: HTMLCanvasElement = undefined;
 
     constructor(props: PlotterProps) {
         super(props);
@@ -171,7 +261,7 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
     render() {
         this.canvas = (
             <canvas ref={(canvasRef) => this.canvasRef = canvasRef} width={Plotter.WIDTH * Plotter.RES_FACTOR}
-                    height={Plotter.HEIGHT * Plotter.RES_FACTOR} style={Plotter.CANVAS_STYLE}>Canvas not
+                height={Plotter.HEIGHT * Plotter.RES_FACTOR} style={Plotter.CANVAS_STYLE}>Canvas not
                 supported.</canvas>
         );
         return this.canvas;
@@ -179,9 +269,9 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
 
     componentDidMount() {
         const ctx = this.canvasRef.getContext('2d');
-        this.setState({ctx: ctx}, () => {
+        this.canvasRef.addEventListener('mousemove', this.handleMouseMove.bind(this));
+        this.setState({ ctx: ctx }, () => {
             ctx.scale(Plotter.RES_FACTOR, Plotter.RES_FACTOR);
-
             this.draw();
         });
     }
@@ -190,7 +280,7 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
         this.draw();
     }
 
-    componentWillReceiveProps(nextProps : PlotterProps) {
+    componentWillReceiveProps(nextProps: PlotterProps) {
         this.setState({
             parametricFunctions: nextProps.parametricFunctions,
             vectorFields: nextProps.vectorFields,
@@ -204,6 +294,7 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
         }
 
         const ctx = this.state.ctx;
+        const cs = this.state.cs;
 
         ctx.clearRect(0, 0, Plotter.WIDTH, Plotter.HEIGHT);
 
@@ -220,11 +311,30 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
         if (this.props.vectorFields) {
             this.props.vectorFields.forEach(f => {
                 if (f) {
-                    this.drawVectorField(f);
-                    //this.drawFieldLine(f, -.25, .25, .01, 500);
+                    this.drawVectorField(f.field);
+                    f.fieldLines.forEach((coords) => {
+                        const x = coords[0];
+                        const y = coords[1];
+                        this.drawBidirectionalFieldLine(f.field, x, y, f.fieldLineStepSize || Plotter.FIELD_LINE_STEP_FACTOR, f.fieldLineSteps || Plotter.FIELD_LINE_STEPS);
+                    });
                 }
             });
         }
+
+        ctx.save();
+        ctx.fillStyle = '#555555';
+
+        if (this.props.plotPoints) {
+            this.props.plotPoints.forEach(coords => {
+                const x = coords[0];
+                const y = coords[1];
+                ctx.beginPath();
+                ctx.arc(cs.x(x), cs.y(y), 4, 0, 2 * math.pi);
+                ctx.fill();
+            });
+        }
+
+        ctx.restore();
     }
 
     drawAxes() {
@@ -290,7 +400,8 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
      * @param {number} y
      */
     drawVector(v: Vector2D, x: number, y: number) {
-        const length = Plotter.VECTOR_LENGTH * math.log10(v.magnitude() + 1) * 2;
+        //const length = Plotter.VECTOR_LENGTH * math.log10(v.magnitude() + 1) * 2;
+        const length = Plotter.VECTOR_LENGTH;
         const scaledV = v.unitVector().transform(x => x * length);
 
         const ctx = this.state.ctx;
@@ -306,7 +417,7 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
      * Draw a vector field
      * @param {VectorField} f
      */
-    drawVectorField(f : VectorField) {
+    drawVectorField(f: VectorField) {
         const ctx = this.state.ctx;
         const cs = this.state.cs;
 
@@ -320,12 +431,24 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
 
         for (let x = startX; x <= endX; x = x + step) {
             for (let y = startY; y <= endY; y = y + step) {
-                this.drawVector(f(x,y), x, y);
+                this.drawVector(f(x, y), x, y);
             }
         }
     }
 
-    drawFieldLine(f : VectorField, startX: number, startY: number, stepFactor: number, steps: number) {
+    /**
+     * Draw a flow line of a vector field from a starting point
+     * @param f the vector field
+     * @param startX the starting x of the line
+     * @param startY the starting y of the line
+     * @param stepFactor the amount by which to multiply the added vectors
+     * @param steps the number of steps to take
+     */
+    drawFieldLine(f: VectorField, startX: number, startY: number, stepFactor: number, steps: number) {
+        let g = (x, y) => {
+            return f(x, y).unitVector();
+        }
+        //g = f;
         const ctx = this.state.ctx;
         const cs = this.state.cs;
 
@@ -336,15 +459,41 @@ export class Plotter extends React.Component<PlotterProps, PlotterState> {
         let y = startY;
 
         for (let stepsElapsed = 0; stepsElapsed < steps; stepsElapsed++) {
-            const newX = x + f(x,y).x * stepFactor;
-            const newY = y + f(x,y).y * stepFactor;
+            const newX = x + g(x, y).x * stepFactor;
+            const newY = y + g(x, y).y * stepFactor;
 
             x = newX;
             y = newY;
 
-            ctx.lineTo(cs.x(x),cs.y(y));
+            ctx.lineTo(cs.x(x), cs.y(y));
         }
 
         ctx.stroke();
+    }
+
+    /**
+     * Draw a flow line of a vector field in both directions from a starting point
+     * @param f the vector field
+     * @param startX the starting x of the line
+     * @param startY the starting y of the line
+     * @param stepFactor the amount by which to multiply the added vectors
+     * @param steps the number of steps to take
+     */
+    drawBidirectionalFieldLine(f: VectorField, startX: number, startY: number, stepFactor: number, steps: number) {
+        this.drawFieldLine(f, startX, startY, stepFactor, steps);
+        this.drawFieldLine(VectorField.multiply(f, -1), startX, startY, stepFactor, steps);
+    }
+
+    /**
+     * Handle when the cursor is moved over the canvas
+     * @param event 
+     */
+    handleMouseMove(event: MouseEvent) {
+        const rect = this.canvasRef.getBoundingClientRect();
+        const cs = this.state.cs;
+
+        if (this.props.onMouseMove) {
+            this.props.onMouseMove(cs.invX(event.clientX - rect.left), cs.invY(event.clientY - rect.top));
+        }
     }
 }
--- client/ts/electric.tsx
+++ client/ts/electric.tsx
@@ -1,14 +1,25 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 
-import {PFZoomInput} from "./ParametricFunctionConfig";
+import { PFZoomInput } from "./ParametricFunctionConfig";
 
-import {Plotter, Vector2D, VectorField} from './Plotter';
+import { Plotter, Vector2D, VectorField } from './Plotter';
+
+import * as math from 'mathjs';
 
 /**
- * Base app component
+ * Base app component for electric field demo
  */
-class VectorFieldApp extends React.Component<{}, { zoom: number, unit: number, fieldType: string }> {
+class VectorFieldApp extends React.Component<{}, {
+    zoom: number
+    unit: number
+    fieldType: string
+    startX: number
+    startY: number
+    mouseX: number
+    mouseY: number
+    lengthFactor: number
+}> {
 
     /**
      * Returns the electric field created by a point charge at the given coordinates (electric constant ignored)
@@ -16,63 +27,129 @@ class VectorFieldApp extends React.Component<{}, { zoom: number, unit: number, f
      * @param {number} xA
      * @param {number} yA
      */
-    static chargeField(q: number, xA: number, yA: number) : VectorField {
+    static chargeField(q: number, xA: number, yA: number): VectorField {
         return (x, y) => {
             const v = new Vector2D(x - xA, y - yA);
             return v.transform(z => z * q / v.magnitude() ** 2);
         }
     }
 
+    /**
+     * Object containing the selectable field presets
+     */
     static readonly fieldTypes = {
-        // single: (x, y) => {
-        //     const v = new Vector2D(x, y);
-        //     const vReduced = v.transform(z => z / v.magnitude() ** 2);
-        //     return vReduced;
-        // },
+        /**
+         * Field produced by a single positive point charge
+         */
         single: VectorFieldApp.chargeField(1, 0, 0),
+
+        /**
+         * Field produced by a positive and negative point charge of equal magnitudes
+         */
         dipole: VectorField.subtract(VectorFieldApp.chargeField(1, -1, 0), VectorFieldApp.chargeField(1, 1, 0)),
+
+        /**
+         * Field produced by two equal positive charges
+         */
         double: VectorField.add(VectorFieldApp.chargeField(1, -1, 0), VectorFieldApp.chargeField(1, 1, 0)),
+
+        /**
+         * Field produced by a positive charge and a smaller negative charge
+         */
         imbalancedDipole: VectorField.subtract(VectorFieldApp.chargeField(2, -1, 0), VectorFieldApp.chargeField(1, 1, 0))
     };
-
 
     constructor(props) {
         super(props);
         this.state = {
             zoom: 0.224,
             unit: 80,
-            fieldType: "single"
+            fieldType: "single",
+            startX: .25,
+            startY: .75,
+            lengthFactor: .25,
+            mouseX: 1,
+            mouseY: 1
         }
     }
 
     render() {
+        // const startX = (this.state.startX - .5) * 5;
+        // const startY = (this.state.startY - .5) * 5;
+
+        const mouseX = this.state.mouseX;
+        const mouseY = this.state.mouseY;
+
         return (<div>
             <h1>Electric Field Plotter</h1>
-            <div><Plotter
-                vectorFields={[VectorFieldApp.fieldTypes[this.state.fieldType] || VectorFieldApp.fieldTypes.single]}
-                unit={this.state.unit}/></div>
             <div>
-                <PFZoomInput label="Zoom: " value={this.state.zoom} onValueChange={this.handleZoomChange.bind(this)}/>
-                <br/><label>Field: <select value={this.state.fieldType}
-                                           onChange={this.handleFieldTypeChange.bind(this)}>
-                <option value="single">Single Charge</option>
-                <option value="dipole">Dipole</option>
-                <option value="double">Double Charge</option>
-                <option value="imbalancedDipole">Imbalanced Dipole</option>
-            </select></label>
+                <PFZoomInput label="Zoom: " value={this.state.zoom} onValueChange={this.handleZoomChange.bind(this)} />
+                {/* <br /><PFZoomInput label="Field Line Start X: " value={this.state.startX} onValueChange={this.handleStartXChange.bind(this)} /> {startX.toFixed(2)} */}
+                {/* <br /><PFZoomInput label="Field Line Start Y: " value={this.state.startY} onValueChange={this.handleStartYChange.bind(this)} /> {startY.toFixed(2)} */}
+                <br /><PFZoomInput label="Field Line Length" value={this.state.lengthFactor} onValueChange={this.handleLengthChange.bind(this)} />
+                <br /><label>Field: <select value={this.state.fieldType}
+                    onChange={this.handleFieldTypeChange.bind(this)}>
+                    <option value="single">Single Charge</option>
+                    <option value="dipole">Dipole</option>
+                    <option value="double">Double Charge</option>
+                    <option value="imbalancedDipole">Imbalanced Dipole</option>
+                </select></label>
             </div>
+            <div><Plotter
+                vectorFields={[{
+                    field: VectorFieldApp.fieldTypes[this.state.fieldType] || VectorFieldApp.fieldTypes.single,
+                    // fieldLines: [[startX, startY]],
+                    fieldLines: [[mouseX,mouseY]],
+                    fieldLineSteps: 100 + this.state.lengthFactor * 1000
+                }]} plotPoints={[[mouseX, mouseY]]} unit={this.state.unit} onMouseMove={this.handleMouseMove.bind(this)}/></div>
         </div>);
     }
 
+    /**
+     * Handle changes to the zoom control
+     * @param zoom Ranges from 0 to 1
+     */
     handleZoomChange(zoom: number) {
-        this.setState({unit: zoom * zoom * 40 * 40});
+        // Set the unit of the coordinate system
+        this.setState({ unit: zoom * zoom * 40 * 40 });
     }
 
+    // /**
+    //  * Handle changes to the start X control
+    //  * @param xFactor 0-1
+    //  */
+    // handleStartXChange(xFactor: string) {
+    //     this.setState({ startX: parseFloat(xFactor) });
+    // }
+
+    // /**
+    // * Handle changes to the start Y control
+    // * @param yFactor 0-1
+    // */
+    // handleStartYChange(yFactor: string) {
+    //     this.setState({ startY: parseFloat(yFactor) });
+    // }
+
+    handleLengthChange(lengthFactor: string) {
+        this.setState({ lengthFactor: parseFloat(lengthFactor) });
+    }
+
+    /**
+     * Handle changes to the field preset selector
+     * @param event 
+     */
     handleFieldTypeChange(event) {
-        this.setState({fieldType: event.target.value});
+        /**
+         * Set the new field type
+         */
+        this.setState({ fieldType: event.target.value });
+    }
+
+    handleMouseMove(x: number, y: number) {
+        this.setState({mouseX: x, mouseY: y});
     }
 }
 
 // Render the base component in the root div
-ReactDOM.render(<VectorFieldApp/>, document.getElementById('root'));
+ReactDOM.render(<VectorFieldApp />, document.getElementById('root'));
 
